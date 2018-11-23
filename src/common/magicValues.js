d13229d2eba3eef3670f66880455bca9d1225234
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-11-23 03:48:28 +0200

    - added default routing load from server

--- src/common/magicValues.js
+++ src/common/magicValues.js
@@ -1,3 +1,6 @@
 export const ACTIONS = {
     ROUTING_LOADED: 'ROUTING_LOADED'
 };
+
+export const SERVER_URL = 'http://localhost:8509';
+export const SERVER_URL_ROUTING = `${SERVER_URL}/routing`;
--- src/store.js
+++ src/store.js
@@ -3,7 +3,10 @@ import { applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from './reducers';
+import { loadRouting } from "./actions";
+import { SERVER_URL_ROUTING } from "./common/magicValues";
 
 const store = createDynamicStore({ rootReducer }, applyMiddleware(thunk));
+store.dispatch(loadRouting(SERVER_URL_ROUTING));
 
 export default store;
